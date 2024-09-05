4261464bfb97a0dbf0c457308e1c216787cd5826
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-05 19:24:18 +0300

    feat: :recycle: Add useLoading hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,21 @@ import Spinner from './components/Spinner.jsx';
 import PlayerInfo from './components/PlayerInfo.jsx';
 import Avatar from './assets/avatar.png';
 import Loader from './components/Loader.jsx';
-import { useEffect } from 'react';
 import TournamentItem from './components/TournamentItem.jsx';
 import tournaments from './data/tournaments.js';
+import useLoading from './hooks/useLoading.js';
 
 function App() {
-  const [loading, setLoading] = React.useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLoading(prev => (prev < 100 ? prev + 1 : 100));
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []);
+  const {percent, isLoaded} = useLoading(0);
 
   return (
     <div className='app'>
       <div className='hero'>
         <h1>Poker</h1>
-        {loading < 100 && <Spinner />}
+        {isLoaded && <Spinner />}
       </div>
       <PlayerInfo name="LongUserName" rating={5} avatar={Avatar} />
-      {loading < 100 && <Loader loading={loading} />}
+      {isLoaded && <Loader loading={percent} />}
       <div className="grid">
         {tournaments.list.map((item, index) => <TournamentItem key={item.id} {...item} />)}
       </div>
