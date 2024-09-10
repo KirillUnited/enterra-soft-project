0542cec6ea3fbf1664f65e3116451e0860c1f977
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-10 14:32:58 +0300

    feat(head): :sparkles: Add SEO components

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,33 @@ import Loader from './components/Loader.jsx';
 import TournamentItem from './components/TournamentItem.jsx';
 import tournaments from './data/tournaments.js';
 import useLoading from './hooks/useLoading.js';
+import SEO from './components/SEO.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   const { percent, isLoaded } = useLoading(0);
   const [selectedEl, setSelectedEl] = React.useState(false);
 
   return (
-    <div className='app'>
-      <div className='hero'>
-        <h1>Poker</h1>
-        {isLoaded && <Spinner />}
+    <HelmetProvider>
+      <div className='app'>
+        <SEO />
+        <div className='hero'>
+          <h1>Poker</h1>
+          {isLoaded && <Spinner />}
+        </div>
+        <PlayerInfo name="LongUserName" rating={5} avatar={Avatar} />
+        {isLoaded && <Loader loading={percent} />}
+        <div className="grid">
+          {
+            !isLoaded &&
+            tournaments.list.map((item, index) => {
+              return <TournamentItem key={item.id} {...item} selectedEl={selectedEl} setSelectedEl={setSelectedEl} />
+            })
+          }
+        </div>
       </div>
-      <PlayerInfo name="LongUserName" rating={5} avatar={Avatar} />
-      {isLoaded && <Loader loading={percent} />}
-      <div className="grid">
-        {
-          !isLoaded &&
-          tournaments.list.map((item, index) => {
-            return <TournamentItem key={item.id} {...item} selectedEl={selectedEl} setSelectedEl={setSelectedEl} />
-          })
-        }
-      </div>
-    </div>
+    </HelmetProvider>
   )
 }
 
